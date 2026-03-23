eec9c86456af7fc753d91d7d96039ab67f46cd85
Manos <manosnikitakis3@gmail.com>
2026-03-23 11:23:29 +0200

    add function for generating room code

---
+++ frontend/src/utils/generateCode.ts
@@ -0,0 +1,6 @@
+export function generateRoomCode(): string {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  return Array.from({ length: 6 }, () =>
+    chars[Math.floor(Math.random() * chars.length)]
+  ).join('')
+}
