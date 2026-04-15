b1234ebd07b23c408c12c4767d281ba0c0ad04c9
Manos <manosnikitakis3@gmail.com>
2026-04-15 11:44:55 +0300

    add ClerkProvider, BrowserRouter, AuthProvider and SocketProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import AuthProvider from './providers/AuthProvider.tsx'
+import { SocketProvider } from './contexts/SocketContext.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={publishableKey ?? ''}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <SocketProvider>
+              <App />
+            </SocketProvider>
+          </AuthProvider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 )
